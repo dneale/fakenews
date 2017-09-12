f40cd2455d60db7d9b65dca7c2b5b27b7104b475
Doug <dougneale@gmail.com>
2017-09-12 21:59:34 +1000

    Basic ui started

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import Convergence from "./Convergence";
 
 ReactDOM.render(
-  <App />,
+  <Convergence />,
   document.getElementById('root')
 );
