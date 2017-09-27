835d6d2a8b6cff72c5b535cc7628cef888e79cb2
Doug Neale <doug.neale@originenergy.com.au>
2017-09-27 21:00:13 +1000

    added basic game

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import faker from 'faker';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,50 +8,130 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      fetching: true
+      prevQuestion: null,
+      prevAnswer: null,
+      currentAnswer: {
+        computer: faker.random.word(),
+        you: "",
+      },
+      usedWords: [],
+      loading: "",
+      error: "",
+      convergence: false,
     };
   }
 
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
+  pickRandom = () => {
+    const random = faker.random.word();
+    this.setState({
+      currentAnswer: {
+        ...this.state.currentAnswer,
+        you: random,
+      }
+    });
+    this.updateGame(random);
   }
+
+  submitWord = (event) => {
+    event.preventDefault();
+    this.updateGame(this.state.currentAnswer.you);
+  };
+
+  updateGame(word) {
+
+    const currentAnswer = {
+      computer: this.state.currentAnswer.computer,
+      you: word,
+    }
+    if (this.state.usedWords.includes(currentAnswer.you.toLowerCase())) {
+      this.setState({
+        error: "That word has been used already",
+      });
+      return;
+    }
+
+    this.setState({
+      prevAnswer: currentAnswer,
+      prevQuestion: this.state.prevAnswer,
+      currentAnswer: {
+        computer: "",
+        you: "",
+      },
+      usedWords: [
+        ...this.state.usedWords,
+        currentAnswer.you.toLowerCase(),
+        currentAnswer.computer.toLowerCase(),
+      ],
+      error: "",
+    });
+
+    fetch(`https://api.datamuse.com/words?ml=${currentAnswer.computer}+${currentAnswer.you}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then(json => {
+        const topWord = this.getWord(json);
+        this.setState({
+          currentAnswer: {
+            ...this.state.currentAnswer,
+            computer: topWord,
+          },
+        })
+      });
+  };
+
+  getWord = (wordList) => {
+    let unusedWords = wordList.filter((wordObj) => {
+      const word = wordObj.word;
+      if (this.state.usedWords.includes(word.toLowerCase())) {
+        return false;
+      }
+      if (!wordObj.tags || !wordObj.tags.includes('n')) {
+        return false;
+      }
+      return true;
+    });
+    return unusedWords[0].word;
+  };
+
+  updateWord = (event) => {
+    this.setState({
+      currentAnswer: {
+        ...this.state.currentAnswer,
+        you: event.target.value,
+      }
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {'This is '}
-          <a href="https://github.com/mars/heroku-cra-node">
-            {'create-react-app with a custom Node/Express server'}
-          </a><br/>
-        </p>
-        <p className="App-intro">
-          {this.state.fetching
-            ? 'Fetching message from API'
-            : this.state.message}
-        </p>
+        <h1>Convergence with a Computer </h1>
+        {!this.state.prevAnswer &&
+          <p> Enter a word, or <a onClick={this.pickRandom}> choose a random one</a></p>
+        }
+        {this.state.prevAnswer &&
+          <div>
+            {this.state.prevQuestion &&
+            <div>
+              <p> the two words were <b>{this.state.prevQuestion.you}</b> and <b>{this.state.prevQuestion.computer}</b></p>
+            </div>
+            }
+            <p><b>You said </b>{this.state.prevAnswer.you} </p>
+            <p><b>The computer said: </b>{this.state.prevAnswer.computer} </p>
+          </div>
+        }
+        <form>
+          <input type="text" name="word" value={this.state.currentAnswer.you} onChange={this.updateWord} />
+          <input disabled={!this.state.currentAnswer.computer || !this.state.currentAnswer.you} type="submit" onClick={this.submitWord} />
+          <p style={{color:'red'}}>{this.state.error}</p>
+        </form>
+        {this.state.convergence &&
+          <h1 style={{color:'green'}}> CONVERGENCE!</h1>
+        }
       </div>
     );
   }
